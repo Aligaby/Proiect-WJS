0ee1bc23e3a300408105b3156a671bfb6235f2e7
Aligaby <aligaby2000@yahoo.com>
2022-02-13 13:45:58 +0200

    produse (json, fetch) + imagini produse

--- scripts/produs.js
+++ scripts/produs.js
@@ -1,28 +1,34 @@
 const endpointUrl = 'data/content.json';
 const productMain = document.getElementById('productMain');
+const idProdus = Number(location.href.split('id=')[1]);
 
-fetch(endpointUrl).then((rezultat) => rezultat.json()).then(rezultatDate => {
-    const produse = rezultatDate.produse;
+function dateProdus(produs) {
+    const {id, categorie, titlu, descriere, producator, imagine} = produs;
+    const userContainer = document.createElement('article');
+    const idJson = document.createElement('div');
+    const categorieJson = document.createElement('div');
+    const titluJson = document.createElement('div');
+    const descriereJson = document.createElement('div');
+    const producatorJson = document.createElement('div');
+    const imgJson = document.createElement('img');
+
+    idJson.textContent = `${id}`;
+    categorieJson.textContent = `${categorie}`;
+    titluJson.textContent = `${titlu}`;
+    descriereJson.textContent = `${descriere}`;
+    producatorJson.textContent = `${producator}`;
+    imgJson.src = `${imagine}`;
+    userContainer.appendChild(idJson);
+    userContainer.appendChild(categorieJson);
+    userContainer.appendChild(titluJson);
+    userContainer.appendChild(descriereJson);
+    userContainer.appendChild(producatorJson);
+    userContainer.appendChild(imgJson);
+    productMain.appendChild(userContainer);
+} 
+
+fetch(endpointUrl).then((data) => data.json()).then(date => {
+    const produse = date.produse;
     console.log(produse);
-
-    produse.forEach(produs => {
-        const {id, titlu, descriere} = produs;
-        const userContainer = document.createElement('article');
-        const idJson = document.createElement('div');
-        const titluJson = document.createElement('div');
-        const descriereJson = document.createElement('div');
-
-        idJson.textContent = `${id}`;
-        titluJson.textContent = `${titlu}`;
-        descriereJson.textContent = `${descriere}`;
-        userContainer.appendChild(idJson);
-        userContainer.appendChild(titluJson);
-        userContainer.appendChild(descriereJson);
-        productMain.appendChild(userContainer);
-    });
+    dateProdus(produse[idProdus-1]);
 })
-
-// const urlRelativ = location.href.split('/');
-// const urlPagina = urlRelativ[urlRelativ.length-1];
-// const idUrlPagina = urlPagina.substring(0, 2);
-// console.log(+idUrlPagina);
